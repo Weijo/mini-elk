c89827132f570ab4c36a599250211e076104f30b
Wei Jie <tayweijie777@gmail.com>
2023-07-03 06:25:58 -0400

    added submission integration

--- setup_policies.sh
+++ setup_policies.sh
@@ -7,11 +7,31 @@ fi
 
 KIBANA_HOST="127.0.0.1"
 
+# Log a message.
+function log {
+	echo "[+] $1"
+}
+
+# Log a message at a sub-level.
+function sublog {
+	echo "   ⠿ $1"
+}
+
+# Log an error.
+function err {
+	echo "[x] $1" >&2
+}
+
+# Log an error at a sub-level.
+function suberr {
+	echo "   ⠍ $1" >&2
+}
+
 function install_integration {
 	local filename=$1
 
 	local output
-
+	
 	output=$(curl --request POST \
 	-s \
 	--url "https://$KIBANA_HOST:5601/api/fleet/package_policies" \
@@ -28,5 +48,6 @@ function install_integration {
 FILES="./policies/*.json"
 for f in $FILES
 do
+	log "Adding $f"
 	install_integration $f
 done
