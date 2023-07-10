6fd0e279b2f81420d6b18247e22c0ae6fb174ae3
Wei Jie <tayweijie777@gmail.com>
2023-07-10 03:17:32 -0400

    added transforms for CTFd submission time taken

--- setup_transforms.sh
+++ setup_transforms.sh
@@ -5,7 +5,7 @@ if [ -f .env ]; then
 	export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst)
 fi
 
-KIBANA_HOST="127.0.0.1"
+ELASTIC_HOST="192.168.147.144"
 
 # Log a message.
 function log {
@@ -27,33 +27,32 @@ function suberr {
 	echo "   ⠍ $1" >&2
 }
 
-function install_integration {
+function install_transforms {
 	local filename=$1
-
+    local transform_name=$(basename "$filename" .json)
 	local output
 	
-	output=$(curl --request POST \
+	output=$(curl --request PUT \
 	-s \
 	--write-out "%{http_code}" \
-	--url "https://$KIBANA_HOST:5601/api/fleet/package_policies" \
+	--url "https://$ELASTIC_HOST:9200/_transform/$transform_name" \
 	--header 'Content-Type: application/json' \
-	--header 'kbn-xsrf: xxx' \
 	-k \
 	-u elastic:$ELASTIC_PASSWORD \
 	--data @$filename
 	)
 
 	if [[ "${output: -3}" -eq 400 ]]; then 
-		suberr "Integration already exists"
+		suberr "Transform already exists"
 	elif [[ "${output: -3}" -eq 200 ]]; then 
-		sublog "Integration added"
+		sublog "Transform added"
 	fi
 
 }
 
-FILES="./policies/*.json"
+FILES="./transforms/*.json"
 for f in $FILES
 do
 	log "Adding $f"
-	install_integration $f
+	install_transforms $f
 done
